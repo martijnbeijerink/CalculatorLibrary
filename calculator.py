5b7c30913ef5bde83f924ce7d520ded9d9813f69
Martijn Beijerink <Martijn.Beijerink@vanderlande.com>
2020-03-23 17:48:39 +0100

    Add first functions for calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 17:44:12 2020
+
+@author: nlmbeij
+"""
+
+def add(first_term, second term):
+    return first_term+second term
+
+def substract(first_term,second_term):
+    return first_term-second_term)
